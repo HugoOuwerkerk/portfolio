dc8e965822cbd72c9d10dd2660bd6bd58baed03f
HugoOuwerkerk <hah.ouwerkerk@outlook.com>
2025-12-18 23:53:47 +0100

    platzpionier webiste screenshot update

--- app/page.tsx
+++ app/page.tsx
@@ -161,7 +161,7 @@ export default function Home() {
             link="https://app.platzpionier.com"
             color="#3b82f6"
             align="left"
-            image="/platzpionier.png"
+            image="/platzpionier_new.png"
             maskLink={true}
           />
 
